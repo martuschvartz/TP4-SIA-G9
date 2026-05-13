6c4701afcc2309749e9782382d1c8af1ae0b18cb
juane993 <jolivamorroni@itba.edu.ar>
2026-05-13 20:42:07 -0300

    BMU map

--- kohonen/main.py
+++ kohonen/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
 import KohonenMap
@@ -33,6 +34,27 @@ def main():
             kohonen_map.update_weights(X, winner_cords)
             kohonen_map.update_radius()
             kohonen_map.update_learning_rate(epoch)
+        # Count hits per cell
+
+        # Compute BMU hit map AFTER training
+    bmu_coords = []
+    for X in scaled_array:
+        winner_cords = kohonen_map.find_winner_cords(X)
+        bmu_coords.append(winner_cords.get_cords())
+
+    hits = np.zeros((grid_size, grid_size), dtype=int)
+    for x, y in bmu_coords:
+        hits[x, y] += 1
+
+    plt.imshow(hits, cmap="viridis")
+    plt.colorbar(label="hits")
+    plt.title("BMU hit map")
+    plt.savefig("bmu_hit_map.png", dpi=150, bbox_inches="tight")
+    plt.close()
+
+
+
+
 
     # for i in range(1000 * input_len):
     #     X = scaled_array[np.random.randint(len(scaled_array))]
